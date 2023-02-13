bb861e4ae29b707776f03c08912737818f343d9d
Yonathan-Calderon <calderonprog@gmail.com>
2023-02-13 16:10:13 -0300

    Esta con el Mock, falta conectarlo a BB.DD Mongo

--- app.js
+++ app.js
@@ -10,30 +10,23 @@ mongoose.set('strictQuery', false);
 mongoose.connect(
     process.env.DB_URL
 );
-
 const db = mongoose.connection;
 db.on('error', error=> console.error(error));
 db.once('open', ()=> console.log('Conectado a BB.DD'));
 
+//IMPORT ROUTES
+const itemsRoutes = require('./routes/items');
+const imgRoutes = require('./routes/img');
 
-
-//MOCK
-const itemsMock = require('./mock/items.json');
 
 //MIDDLWARE
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+app.use('/items',itemsRoutes);
+app.use('/img',imgRoutes );
 //RUTAS
 app.get('/', (req, res)=> {
     res.send('HOME!');
-});
-//ITEMS
-app.get('/items', (req, res)=> {
-    res.send(itemsMock)
-});
-//IMAGES
-app.get('/img/:imgName', (req, res)=>{
-    const image = req.params.imgName; 
-    res.sendFile(`${__dirname}/img/${image}`);
 });
 
 
---
+++ routes/items.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+//MODELS
+const Item = require('../models/Item');
+
+//MOCK
+const itemsMock = require('../mock/items.json');
+
+//ITEMS
+router.get('/', (req, res)=> {
+    res.send(itemsMock)
+});
+
+// CREATE ITEM
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    const item = new Item({
+      title: req.body.title,
+      price: req.body.price,
+      image: req.body.image,
+    });
+    try {
+      const newItem = await item.save();
+      res.json(newItem);
+    } catch (err) {
+      res.json({ message: err.message });
+    }
+  });
+
+
+module.exports = router;
