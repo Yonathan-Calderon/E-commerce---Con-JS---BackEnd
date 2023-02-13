4b88191e9fd048e0f74a8d950b134980be5a46bc
Yonathan-Calderon <calderonprog@gmail.com>
2023-02-13 11:54:30 -0300

    Hasta la conexion con DB

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-//Rutas
-app.get
+require('dotenv').config();
+
+//BBDD
+mongoose.set('strictQuery', false);
+mongoose.connect(
+    process.env.DB_URL
+);
+
+const db = mongoose.connection;
+db.on('error', error=> console.error(error));
+db.once('open', ()=> console.log('Conectado a BB.DD'));
+
+
+
+//MOCK
+const itemsMock = require('./mock/items.json');
+
+//MIDDLWARE
+app.use(cors())
+//RUTAS
+app.get('/', (req, res)=> {
+    res.send('HOME!');
+});
+//ITEMS
+app.get('/items', (req, res)=> {
+    res.send(itemsMock)
+});
+//IMAGES
+app.get('/img/:imgName', (req, res)=>{
+    const image = req.params.imgName; 
+    res.sendFile(`${__dirname}/img/${image}`);
+});
+
+
+//START
+app.listen(3000);
